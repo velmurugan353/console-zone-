4a96312d0d0bd353dd4c5b503400fd20da5bd99e
velmurugan <murugan.cheers@gmail.com>
2026-05-08 23:57:38 +0530

    Update Google Client ID

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App.tsx';
 import './index.css';
 
-const GOOGLE_CLIENT_ID = "27387199701-jlr9nesfd0tue8knekc8kk6prnsrfr5f.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = "313866824968-q89hk1l6mv1r7sbn29o5j3i738nlc1cp.apps.googleusercontent.com";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
